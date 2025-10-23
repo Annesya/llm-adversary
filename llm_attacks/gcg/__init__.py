3d2ddf270092caf0b67dbf314b7685fac9273ae2
Annesya <banerjee.annesya1999@gmail.com>
2025-10-23 11:03:32 -0400

    updated modules and configs with coherence based losses

--- llm_attacks/gcg/__init__.py
+++ llm_attacks/gcg/__init__.py
@@ -1,6 +1,8 @@
 from .gcg_attack import GCGAttackPrompt as AttackPrompt
 from .gcg_attack import GCGPromptManager as PromptManager
 from .gcg_attack import GCGMultiPromptAttack as MultiPromptAttack
+from .gcg_attack import GCGMultiPromptAttackCoherence as MultiPromptAttackCoherence
 
 from llm_attacks import ProgressiveMultiPromptAttack
 from llm_attacks import IndividualPromptAttack
+from llm_attacks import ProgressiveMultiPromptAttackCoherence
